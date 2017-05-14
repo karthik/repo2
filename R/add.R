522987cb8e92d29cf4c0504644e3c38243efe942
Karthik Ram <karthik.ram@gmail.com>
2017-05-13 18:13:41 -0700

    Added travis docs

---
+++ R/add.R
@@ -0,0 +1,14 @@
+
+
+#' This function adds two variables
+#'
+#' @param x variable1 
+#' @param y  variable2
+#'
+#' @export
+#'
+#' @examples
+#' gadd(1,2)
+gadd <- function(x,y) {
+  x + y
+}
---
+++ tic.R
@@ -0,0 +1,14 @@
+#before_script
+before_script:
+  - R -q -e 'devtools::install_github("ropenscilabs/tic"); tic::prepare_all_stages()'
+
+#after_success
+after_success:
+  - R -q -e 'tic::after_success()'
+
+#deploy
+deploy:
+  provider: script
+script: R -q -e 'tic::deploy()'
+on:
+  all_branches: true
