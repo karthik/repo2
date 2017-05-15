f2c8b5a3ba3f35bc7da8385adc473ecc35d03b11
Karthik Ram <karthik.ram@gmail.com>
2017-05-15 15:19:58 +0100

    Fixing

--- tic.R
+++ tic.R
@@ -1,14 +1,15 @@
 get_stage("after_success") %>%
   add_step(step_install_ssh_keys()) %>%
-  add_step(step_build_pkgdown()) %>%
-  add_step(step_push_deploy(path = "docs", branch = "gh-pages"))
-
-
-get_stage("deploy") %>%
-  add_step(step_install_ssh_keys()) %>%
   add_step(step_test_ssh()) %>%
   add_step(step_build_pkgdown()) %>%
   add_step(step_push_deploy(path = "docs", branch = "gh-pages"))
+
+
+# get_stage("deploy") %>%
+#   add_step(step_install_ssh_keys()) %>%
+#   add_step(step_test_ssh()) %>%
+#   add_step(step_build_pkgdown()) %>%
+#   add_step(step_push_deploy(path = "docs", branch = "gh-pages"))
 
 
 
