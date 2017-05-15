3c3dbd7d92c25ac4757e1fb664156dac82faf407
Karthik Ram <karthik.ram@gmail.com>
2017-05-15 12:31:55 +0100

    Doing something simpler

--- tic.R
+++ tic.R
@@ -1,5 +1,5 @@
 get_stage("after_success") %>%
- add_step(step_build_pkgdown()) % >%
+ add_step(step_build_pkgdown()) %>%
     add_step(step_push_deploy(path  =  "docs",branch  =  "gh-pages"))
 
 # get_stage("deploy") %>%
