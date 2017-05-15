a5cad36ab002d8a2e444f40bd712cba9a884edf4
Karthik Ram <karthik.ram@gmail.com>
2017-05-15 10:29:14 +0100

    Updated tic

--- tic.R
+++ tic.R
@@ -1,14 +1,14 @@
 library(magrittr)
-get_stage("after_success") % >%
-  add_step(step_hello_world()) % >%
+get_stage("after_success") %>%
+  add_step(step_hello_world()) %>%
   add_step(step_run_covr())
 
-get_stage("deploy") % >%
-  add_step(step_install_ssh_keys()) % >%
+get_stage("deploy") %>%
+  add_step(step_install_ssh_keys()) %>%
   add_step(step_test_ssh())
 
 if  (ci()$get_branch()==  "production"  &&  Sys.getenv("BUILD_PKGDOWN")!=  "") {
-  get_stage("deploy") % >%
-    add_step(step_build_pkgdown()) % >%
+  get_stage("deploy") %>%
+    add_step(step_build_pkgdown()) %>%
     add_step(step_push_deploy(path  =  "docs",branch  =  "gh-pages"))
 }
