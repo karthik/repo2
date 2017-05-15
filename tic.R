dc3616a6d1c4f31aa0f5e61120373435eed95406
Karthik Ram <karthik.ram@gmail.com>
2017-05-15 13:44:06 +0100

    Adding ssh keys

--- tic.R
+++ tic.R
@@ -1,6 +1,8 @@
 get_stage("after_success") %>%
- add_step(step_build_pkgdown()) %>%
-    add_step(step_push_deploy(path  =  "docs",branch  =  "gh-pages"))
+  add_step(step_install_ssh_keys()) %>%
+  add_step(step_build_pkgdown()) %>%
+  add_step(step_push_deploy(path = "docs", branch = "gh-pages"))
+
 
 # get_stage("deploy") %>%
 #   add_step(step_install_ssh_keys()) %>%
